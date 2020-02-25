d5b0ff2e3b54d9315cd83d5ae699bbda58b076e5
Jason Long <jlong5795@gmail.com>
2020-02-25 00:12:41 -0600

    Final commit

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const router = require('express').Router();
 
 const Users = require('../users/users-model.js');
@@ -5,7 +6,9 @@ const Users = require('../users/users-model.js');
 router.post('/register', (req, res) => {
   let user = req.body;
 
-  const hash = 
+  const hash = bcrypt.hashSync(user.password, 8);
+
+  user.password = hash;
 
   Users.add(user)
     .then(saved => {
@@ -22,7 +25,7 @@ router.post('/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
